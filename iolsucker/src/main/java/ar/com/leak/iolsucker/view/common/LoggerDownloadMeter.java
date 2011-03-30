67efe3c782f6bcd77d53e8f4767a38d57402442e
Juan F. Codagnone <juan@zaubersoftware.com>
2011-03-30 13:01:06 -0300

    Actualizo plugin de license + licencia + pom bare segun nuevos lineamientos de Zauber

--- iolsucker/src/main/java/ar/com/leak/iolsucker/view/common/LoggerDownloadMeter.java
+++ iolsucker/src/main/java/ar/com/leak/iolsucker/view/common/LoggerDownloadMeter.java
@@ -1,22 +1,18 @@
-/* ====================================================================
- *   Copyright 2001-2004 The Apache Software Foundation.
+/**
+ * Copyright (c) 2005-2011 Juan F. Codagnone <http://juan.zaubersoftware.com>
  *
- *   Licensed under the Apache License, Version 2.0 (the "License");
- *   you may not use this file except in compliance with the License.
- *   You may obtain a copy of the License at
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *     http://www.apache.org/licenses/LICENSE-2.0
  *
- *   Unless required by applicable law or agreed to in writing, software
- *   distributed under the License is distributed on an "AS IS" BASIS,
- *   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *   See the License for the specific language governing permissions and
- *   limitations under the License.
- * ====================================================================
- * 
- * Modified by Juan F. Codagnone to use Logger instead of System.out
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
-
 package ar.com.leak.iolsucker.view.common;
 
 import org.slf4j.Logger;
